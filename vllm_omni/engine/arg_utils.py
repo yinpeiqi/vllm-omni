d7ab8ca7ce8c26748ebf1021f8e91dbd8fc6b123
yinpe <11810305@mail.sustech.edu.cn>
2026-03-10 16:09:54 +0800

    rebase
    Signed-off-by: yinpe <11810305@mail.sustech.edu.cn>

--- vllm_omni/engine/arg_utils.py
+++ vllm_omni/engine/arg_utils.py
@@ -115,6 +115,7 @@ class OmniEngineArgs(EngineArgs):
         self._ensure_omni_models_registered()
 
         # Keep compatibility when async args are constructed from partial payloads.
+        language_model_only = getattr(self, "language_model_only", False)
         limit_mm_per_prompt = getattr(self, "limit_mm_per_prompt", {})
         enable_mm_embeds = getattr(self, "enable_mm_embeds", False)
         interleave_mm_strings = getattr(self, "interleave_mm_strings", False)
@@ -169,6 +170,7 @@ class OmniEngineArgs(EngineArgs):
             skip_tokenizer_init=self.skip_tokenizer_init,
             enable_prompt_embeds=self.enable_prompt_embeds,
             served_model_name=self.served_model_name,
+            language_model_only=language_model_only,
             limit_mm_per_prompt=limit_mm_per_prompt,
             enable_mm_embeds=enable_mm_embeds,
             interleave_mm_strings=interleave_mm_strings,
@@ -275,6 +277,7 @@ class AsyncOmniEngineArgs(AsyncEngineArgs):
         self._ensure_omni_models_registered()
 
         # Keep compatibility when async args are constructed from partial payloads.
+        language_model_only = getattr(self, "language_model_only", False)
         limit_mm_per_prompt = getattr(self, "limit_mm_per_prompt", {})
         enable_mm_embeds = getattr(self, "enable_mm_embeds", False)
         interleave_mm_strings = getattr(self, "interleave_mm_strings", False)
@@ -329,6 +332,7 @@ class AsyncOmniEngineArgs(AsyncEngineArgs):
             skip_tokenizer_init=self.skip_tokenizer_init,
             enable_prompt_embeds=self.enable_prompt_embeds,
             served_model_name=self.served_model_name,
+            language_model_only=language_model_only,
             limit_mm_per_prompt=limit_mm_per_prompt,
             enable_mm_embeds=enable_mm_embeds,
             interleave_mm_strings=interleave_mm_strings,
